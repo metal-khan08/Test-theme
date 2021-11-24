94141f5fcac1f298632e0c7e7f1b964792d4e257
talhaahmad08 <talha@wpminds.com>
2021-11-24 18:48:23 +0500

    Custom query pagination and Events page

--- page-past-events.php
+++ page-past-events.php
@@ -4,17 +4,35 @@ get_header();?>
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-          <p>See What is going on with our World</p>
+          <p>Recap of our Past events</p>
         </div>
       </div>
     </div>
 
 <div class="container container--narrow page-section">
   <?php 
-  while(have_posts()){
-    the_post();
+  $today=date('Ymd');
+  $pastEvents=new WP_Query(array(
+    'paged'=> get_query_var('paged',1),
+    'posts_per_page' => 10,
+    'post_type'=>'event',
+    'meta_key'=>'event_date',
+    'orderby'=>'meta_value_num',
+    'order'=>'ASC',
+    'meta_query'=>array(
+      array(
+        'key'=>'event_date',
+        'compare'=>'<',
+        'value'=>$today,
+        'type'=>'numeric'
+
+      )
+    )
+  ));
+  while($pastEvents->have_posts()){
+    $pastEvents->the_post();
   ?>
     <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
@@ -31,10 +49,11 @@ get_header();?>
               </div>
   <?php
   }
-  echo paginate_links();
+  echo paginate_links(array(
+      'total'=>$pastEvents->max_num_pages
+  ));
   ?>
-<hr class="section-break">
-<p>looking for a recap of past events?<a href="<?php echo site_url('/past-events') ?>"> check out our past events events archive</a></p>
+
 </div>
 <?php get_footer();
 ?>
