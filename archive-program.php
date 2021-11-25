cf5fba67ef95a6fa7c20ce4f5e6a21c2630375a8
talhaahmad08 <talha@wpminds.com>
2021-11-25 18:44:46 +0500

    Events Post types program post types professor post types cleaner code

--- archive-program.php
+++ archive-program.php
@@ -1,15 +1,13 @@
 <?php
-get_header();?>
+get_header();
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Programs</h1>
-        <div class="page-banner__intro">
-          <p>There is something for everyone</p>
-        </div>
-      </div>
-    </div>
+pageBanner(array(
+  'title'=>'All Programs',
+  'subtitle'=>'There is Something for every one'
+));
+?>
+
+
 
 <div class="container container--narrow page-section">
   
